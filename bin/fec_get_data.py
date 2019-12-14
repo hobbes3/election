3d43df728c27a1d31ae1ff3b63f89574590bd848
Satoshi Kawasaki <hobbes3@gmail.com>
2019-12-14 11:55:25 +0000

    Added schedule e total (with the original individual), added sample mode (up to 3 pages max), and updated code for new splunk_rest.

--- bin/fec_get_data.py
+++ bin/fec_get_data.py
@@ -15,14 +15,15 @@ def fec():
     }
 
     schedule_a_url = "https://api.open.fec.gov/v1/schedules/schedule_a/"
-    schedule_e_url = "https://api.open.fec.gov/v1/schedules/schedule_e/"
-
+    schedule_a_total_url = "https://api.open.fec.gov/v1/schedules/schedule_a/"
     schedule_a_params = fec_params.copy()
     schedule_a_params.update({
         "is_individual": True,
         "two_year_transaction_period": 2020,
     })
 
+    schedule_e_url = "https://api.open.fec.gov/v1/schedules/schedule_e/"
+    schedule_e_total_url = "https://api.open.fec.gov/v1/schedules/schedule_e/by_candidate/"
     schedule_e_params = fec_params.copy()
     schedule_e_params.update({
         "cycle": 2020,
@@ -31,19 +32,11 @@ def fec():
     })
 
     def get_candidate(candidate):
-        logger.debug("Getting candidate...", extra={"candidate_id": candidate})
-
-        last_indexes = True
-
-        params = schedule_e_params.copy()
-        params["candidate_id"] = candidate
-
         @try_response
-        def send_candidate(r, *args, **kwargs):
+        def send_schedule_e(r):
             r_json = r.json()
             results = r_json["results"]
             pagination = r_json["pagination"]
-            last_indexes = pagination["last_indexes"]
 
             m = meta.copy()
             m["pagination"] = pagination
@@ -56,29 +49,71 @@ def fec():
                     "request_id": r.request_id,
                 }
 
+                sourcetype = "fec_schedule_e_total" if "by_candidate" in r.url else "fec_schedule_e"
+
                 event = {
                     "index": index,
-                    "sourcetype": "fec_schedule_e",
+                    "sourcetype": sourcetype,
                     "source": __file__,
                     "event": result,
                 }
 
                 data += json.dumps(event)
 
-            s.post(hec_url, headers=hec_headers, data=data)
+            if data:
+                s.post(hec_url, headers=hec_headers, data=data)
 
-            return last_indexes
+            return pagination
 
-        while last_indexes:
+        logger.debug("Getting candidate individual schedule e...", extra={"candidate_id": candidate})
+
+        done = False
+        page = 1
+        last_indexes = True
+        params = schedule_e_params.copy()
+        params["candidate_id"] = candidate
+
+        while not done:
             r = s.get(schedule_e_url, params=params)
             meta = {
                 "request_id": r.request_id,
             }
-            last_indexes = send_candidate(r)
+            pagination = send_schedule_e(r)
 
-            if last_indexes:
+            last_indexes = pagination["last_indexes"]
+            page += 1
+            if script_args.sample and page > 3:
+                logger.debug("Sample mode on so limiting to 3 pages.", extra=meta)
+                done = True
+            elif not last_indexes:
+                done = True
+            else:
                 params["last_index"] = last_indexes["last_index"]
                 params["last_expenditure_date"] = last_indexes["last_expenditure_date"]
+
+        logger.debug("Getting candidate total schedule e...", extra={"candidate_id": candidate})
+
+        done = False
+        page = 1
+        params = schedule_e_params.copy()
+        params["candidate_id"] = candidate
+
+        while not done:
+            r = s.get(schedule_e_total_url, params=params)
+            meta = {
+                "request_id": r.request_id,
+            }
+            pagination = send_schedule_e(r)
+
+            pages = pagination["pages"]
+            page += 1
+            if script_args.sample and page > 3:
+                logger.debug("Sample mode on so limiting to 3 pages.", extra=meta)
+                done = True
+            elif page >= pages:
+                done = True
+            else:
+                params["page"] = page
 
     #sr.multiprocess(get_committee, schedule_a_committees)
     sr.multiprocess(get_candidate, schedule_e_candidates)
